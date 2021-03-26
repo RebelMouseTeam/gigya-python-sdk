8f8790ef348abd78e670e61595058be0802f9deb
mr-gauravrai <36273960+mr-gauravrai@users.noreply.github.com>
2021-03-26 17:20:58 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'gigya',
+    version = '3.3.5',
+    url = '',
+    description = 'Gigya Python SDK',
+    packages = find_packages()
+)
