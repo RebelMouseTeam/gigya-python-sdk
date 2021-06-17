d288cdc8cb2c205cbb5c33fb7df52aa3bf0c0f7f
CordiS <sergey.yushkeev@rebelmouse.com>
2021-06-17 15:29:40 +0300

    + bumping to v3.3.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'gigyasdk',
-    version = '3.3.5',
+    version = '3.3.6',
     url = '',
     description = 'Gigya Python SDK',
     packages = find_packages()
